2009217b31a8a73b4f74fa8d10ed22a3ba8b17f0
kaziathardesigns <65167110+kaziathardesigns@users.noreply.github.com>
2020-07-13 19:45:48 +0300

    js corrections

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-console.log("Hello, I'm Kazi and this is my canvas!")
+
 function menutoggle() {
   var x = document.getElementById ('myNavtoggle');
-  x.className += 'responsive';
+
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
